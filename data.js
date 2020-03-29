5a9846f03c6105f46d35cb35435c498359d9b630
shotahorii <shotahorii@horii-no-MacBook-Pro.local>
2020-03-29 12:28:17 +0100

    init

---
+++ data.js
@@ -0,0 +1,7 @@
+const supermarkets = [
+  [53.3401936,-6.2374167,'EuroSpar Barrow Street'],
+  [53.3434368,-6.2410043,'Fresh Grand Canal'],
+];
+const pharmacies = [
+  [53.3427544,-6.2423422,'Gallery Quay Pharmacy'],
+];
